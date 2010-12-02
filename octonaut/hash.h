6274c568179ec7552ed68d6db1e3d64af4f3ca26
Tom Burdick <tburdick@wrightwoodtech.com>
2010-12-02 15:08:33 -0600

    began more hacking on a hash map, really need hash map before a http server can be made to store headers

--- octonaut/hash.h
+++ octonaut/hash.h
@@ -61,63 +61,78 @@ static bool power_of_two(uint64_t x)
     return (x!=0) && ((x&(x-1)) == 0);
 }
 
-static inline void octo_hash_init(octo_hash *hash, octo_hash_function hash_function)
+static inline void octo_hash_init(octo_hash *hashtable, octo_hash_function hash_function)
 {
-    assert(hash->n_hash_bins != 0);
-    assert(power_of_two(hash->n_hash_bins));
+    assert(hashtable->n_hash_bins != 0);
+    assert(power_of_two(hashtable->n_hash_bins));
 }
 
-static inline void octo_hash_destroy(octo_hash *hash)
+static inline void octo_hash_destroy(octo_hash *hashtable)
 {
 }
 
 /**
  * obtain the hash bin number given the number of bins and a hash
  */
-static inline uint64_t octo_hash_nbin(uint64_t bins, uint64_t hash)
+static inline uint64_t octo_hash_nbin(uint64_t bins, uint64_t keyhash)
 {
-    return (hash & (bins-1));
+    return (keyhash & (bins-1));
 }
 
 /**
  * obtain the hash bin given the hash and key
  */
-static inline octo_list * octo_hash_bin(octo_hash *hash, uint64_t hash)
+static inline octo_list * octo_hash_bin(octo_hash *hashtable, uint64_t keyhash)
 {
-    uint64_t nbin = octo_hash_nbin(hash->n_hash_bins, hash);
-    return hash->hash_bins[nbin];
+    uint64_t nbin = octo_hash_nbin(hashtable->n_hash_bins, keyhash);
+    return hashtable->hash_bins[nbin];
 }
 
-static inline uint64_t octo_hash_size(const octo_hash *hash)
+static inline uint64_t octo_hash_size(const octo_hash *hashtable)
 {
     uint64_t size = 0;
-    for(uint64_t i = 0; i < hash->n_hash_bins; ++i)
+    for(uint64_t i = 0; i < hashtable->n_hash_bins; ++i)
     {
-        size += octo_list_size(hash->hash_bins[i]);
+        size += octo_list_size(hashtable->hash_bins[i]);
     }
     return size;
 }
 
+/**
+ * has an entry
+ */
+bool octo_hash_has(octo_hash *hashtable, uint8_t *key, size_t keylen)
+{
+    uint64_t keyhash = hashtable->hash_function(key, keylen);
+    octo_list *list = octo_hash_bin(hashtable->n_hash_bins, keyhash);
+    octo_list_foreach(pos, next, octo_hash_item, list)
+    {
+
+    }
+}
 
 /**
- * put an entry in to the hash table
+ * put an entry in to the hash table, duplicate keys get ignored!
  */
-void octo_hash_put(octo_hash *hash, octo_hash_entry *entry)
+void octo_hash_put(octo_hash *hashtable, octo_hash_entry *entry)
 {
+    uint64_t keyhash = hashtable->hash_function(entry->key, entry->keylen);
 }
 
 /**
  * get an entry from the hash table
  */
-octo_hash_entry * octo_hash_get(octo_hash *hash, uint8_t *key, size_t keylen)
+octo_hash_entry * octo_hash_get(octo_hash *hashtable, uint8_t *key, size_t keylen)
 {
+    uint64_t hash = hash->hash_function(key, keylen);
 }
 
 /**
  * get and remove an entry from the hash table
  */
-octo_hash_entry * octo_hash_pop(octo_hash *hash, uint8_t *key, size_t keylen)
+octo_hash_entry * octo_hash_pop(octo_hash *hashtable, uint8_t *key, size_t keylen)
 {
+    uint64_t hash = hash->hash_function(key, keylen);
 }
 
 /**
---
+++ octowsgi/wsgi.h
@@ -0,0 +1,4 @@
+/**
+ * take a http header object from octohttp and convert it to a pydict, call
+ * in to a python wsgi function
+ */
